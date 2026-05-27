a73cc1e2a218c40cdecb7ac320c229a261ce7177
NateePC <natee.singkong@gmail.com>
2026-05-27 10:32:10 +0700

    fix: replace broken main.go stub with minimal placeholder

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
 package main
 
-import (
-	"github.com/charmbracelet/bubbles/spinner"
-	"github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/huh"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/jackc/pgx/v5"
-	"gopkg.in/yaml.v3"
-)
-
-func main() {
-	// Placeholder to ensure dependencies are direct requires
-	_ = bubbletea.Model(nil)
-	_ = huh.NewForm()
-	_ = spinner.New()
-	_ = lipgloss.NewStyle()
-	_ = pgx.Connect
-	_ = yaml.Node{}
-}
+func main() {}
