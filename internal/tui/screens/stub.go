be67711fcfd1ab9505fb1a9c4f783f999fd57c3f
NateePC <natee.singkong@gmail.com>
2026-05-27 10:43:32 +0700

    feat: add target connection form

--- internal/tui/screens/stub.go
+++ internal/tui/screens/stub.go
@@ -12,7 +12,6 @@ func (s stub) Init() tea.Cmd                        { return nil }
 func (s stub) Update(tea.Msg) (tea.Model, tea.Cmd) { return s, nil }
 func (s stub) View() string                         { return s.label }
 
-func NewTarget(s *tuitypes.AppState) tea.Model     { return stub{"[target]"} }
 func NewOptions(s *tuitypes.AppState) tea.Model    { return stub{"[options]"} }
 func NewDumpOutput(s *tuitypes.AppState) tea.Model { return stub{"[dump_output]"} }
 func NewProgress(s *tuitypes.AppState) tea.Model   { return stub{"[progress]"} }
